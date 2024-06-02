c43c3308977d2e68c171034cc8e961776b231fc3
AnandParmar1 <anand.laughlogiclabs@gmail.com>
2024-06-02 13:26:51 +0530

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -92,7 +92,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/Anandp146/Portfolio"
+                href="https://github.com/Anandp146/Portfolio-App"
                 target="_blank"
                 className="fork-btn-inner"
               >
